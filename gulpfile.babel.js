5b2f9da1eb27f33fec333c674418a8d3d25ee707
tanishi109 <hayakawa@goodpatch.com>
2017-01-29 01:00:31 +0900

    update develop env

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,27 +3,48 @@ import gulpLoadPlugins from "gulp-load-plugins"
 const $ = gulpLoadPlugins();
 
 gulp.task("webserver", function() {
-  gulp.src("./")
+  gulp.src("./dest")
     .pipe($.webserver({
+      host: '0.0.0.0',
       livereload: true,
     }));
 });
 
+gulp.task("assets", () => {
+  gulp.src("./app/assets/**")
+    .pipe($.plumber())
+    .pipe(gulp.dest("dest/assets"))
+});
+
 gulp.task("js", () => {
-  gulp.src("./app/*.js")
+  gulp.src("./app/scripts/*.js")
     .pipe($.plumber())
     .pipe($.babel())
+    .pipe(gulp.dest("dest/scripts"))
+});
+
+gulp.task("html", () => {
+  gulp.src(["./app/views/*.html", "./app/*.html"])
     .pipe(gulp.dest("dest"))
 });
 
 gulp.task("sass", () => {
-  gulp.src("./app/*.scss")
+  gulp.src("./app/styles/*.scss")
     .pipe($.plumber())
     .pipe($.sass())
-    .pipe(gulp.dest("dest"))
+    .pipe(gulp.dest("dest/styles"))
 });
 
-gulp.task("default", ["webserver"], () => {
-  gulp.watch("./app/*.scss", ["sass"]);
-  gulp.watch("./app/*.js", ["js"]);
+gulp.task("default", ["assets", "webserver"], () => {
+  gulp.watch("./app/styles/*.scss", ["sass"]);
+  gulp.watch(["./app/views/*.html", "./app/*.html"], ["html"]);
+  gulp.watch("./app/scripts/*.js", ["js"]);
 });
+
+gulp.task("copy", () => {
+  gulp.src("./dest/**")
+    .pipe(gulp.dest("./"))
+});
+
+gulp.task("build", ["copy"], () => {});
+
