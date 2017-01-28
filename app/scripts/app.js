d71fe9a2247b05cc360d03733fea5d339adf38b5
tanishi109 <hayakawa@goodpatch.com>
2017-01-29 02:02:14 +0900

    :tada: fill image works

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,29 @@
-console.log('called!!')
+let ctx;
+let stage;
+
+// Init
+$(() => {
+  const canvas = document.getElementById('stage');
+  ctx = canvas.getContext('2d');
+
+  const img = new Image();
+  img.src = "../assets/calliePattern.png";
+
+  img.onload = () => {
+    const pattern = ctx.createPattern(img, "");
+
+    ctx.beginPath();
+    ctx.fillStyle = pattern;
+    ctx.rect(10, 10, 20, 20);
+    ctx.stroke();
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.fillStyle = pattern;
+    ctx.arc(40, 30, 10, 0, Math.PI * 2, false);
+    ctx.stroke();
+    ctx.fill();
+  };
+
+});
+
