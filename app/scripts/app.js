84d9531e3f8371ecf892ded8785d731d29538018
tanishi109 <hayakawa@goodpatch.com>
2017-01-29 21:09:18 +0900

    rotation feature

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -43,8 +43,9 @@ class Triangle {
     this.top1 = {x: 100, y: 30};
     this.top2 = {x: 100, y: 75};
     this.top3 = {x: 80, y: 75};
+    this.center = this.getCenter();
 
-    this.rotation = 0;
+    this.rotation = 30;
   }
 
   /**
@@ -53,19 +54,32 @@ class Triangle {
   render() {
     ctx.save();
 
+    ctx.translate(this.center.x, this.center.y);
     ctx.rotate(this.rotation * Math.PI / 180);
 
     ctx.beginPath();
     ctx.fillStyle = pattern;
 
-    ctx.moveTo(this.top1.x, this.top1.y);
-    ctx.lineTo(this.top2.x, this.top2.y);
-    ctx.lineTo(this.top3.x, this.top3.y);
+    ctx.moveTo(this.center.x - this.top1.x, this.center.y - this.top1.y);
+    ctx.lineTo(this.center.x - this.top2.x, this.center.y - this.top2.y);
+    ctx.lineTo(this.center.x - this.top3.x, this.center.y - this.top3.y);
+
+    ctx.rotate(-this.rotation * Math.PI / 180);
     ctx.fill();
 
     ctx.restore();
   }
 
+  /**
+    get center position of triangle
+    @return {object} {x, y}
+  */
+  getCenter() {
+    const x = (this.top1.x + this.top2.x + this.top3.x) / 3;
+    const y = (this.top1.y + this.top2.y + this.top3.y) / 3;
+
+    return {x, y};
+  }
 }
 
 /**
